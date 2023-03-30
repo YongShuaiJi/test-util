7041371fdc1d642d4d0e05a05969b07eb3cb352f
jiyongshuai <jysnana@163.com>
2023-03-31 00:50:19 +0800

    test init

---
+++ src/test/java/org/xmind/transform/XmindTransformTest.java
@@ -0,0 +1,14 @@
+package org.xmind.transform;
+
+import org.junit.Test;
+
+/**
+ * @author jiyongshuai
+ * @email jysnana@163.com
+ * @Date 2023/03/31 00:49:01
+ */
+public class XmindTransformTest {
+
+    @Test
+    public void testXmindTransform(){}
+}
